f2bbf23d3893c8e43926aa61b5ebdfb29d7e5e42
Simon  Date <simondate@yahoo.co.uk>
2017-06-22 15:13:04 +0100

    First working commit.

--- js/adapt-printPage.js
+++ js/adapt-printPage.js
@@ -4,26 +4,47 @@ define([
 
   var printPageView = Backbone.View.extend({
 
-    className: "block printPage",
+    className: "printPage",
 
     initialize: function() {
+      this.listenTo(Adapt, {
+        "navigation:printPage": function(){
+          window.print();},
+      });
+
       this.listenTo(Adapt, 'remove', this.remove);
-      var blocks = this.model.pageModel.findDescendants("blocks");
-      var lastID = blocks.last().get("_id");
-      this.render(lastID);
+      console.log(this.model);
+      console.log(this.model.model._showOn);
+      if(this.model.model._showOn == 'navigation'){
+        this.renderNavigation();
+      } else {
+        var blocks = this.model.pageModel.findDescendants("blocks");
+        var lastID = blocks.last().get("_id");
+        this.renderBottom(lastID);
+      }
+
     },
 
-    render: function(lastID) {
-      var template = Handlebars.templates["printPage"];
+    renderNavigation: function() {
+      var template = Handlebars.templates["printPage-navigation"];
+      console.log(template);
+      console.log(this.$el);
+      $('.navigation-inner').append(this.$el.html(template(this.model.pageModel.attributes._printPage)));
+    },
+
+    renderBottom: function(lastID) {
+      var template = Handlebars.templates["printPage-bottom"];
       $('.' + lastID).after(this.$el.html(template(this.model.pageModel.attributes._printPage)));
       return this;
     }
   });
 
   Adapt.on("pageView:ready", function(pageView) {
-    if (Adapt.findById(Adapt.location._currentId).attributes._printPage._isEnabled !== true) return;
+    var model = Adapt.findById(Adapt.location._currentId).attributes._printPage;
+    if(model == undefined) return;
+    if (model._isEnabled !== true) return;
     var pageModel = pageView.model;
-    new printPageView({model:{ pageModel }});
+    new printPageView({model:{ pageModel, model }});
   });
 
   Adapt.on("menuView:ready", function() {
