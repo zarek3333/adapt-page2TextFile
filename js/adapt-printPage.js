a7e87f2a3692d4fd8ee24cc75f7476105c47ab09
Simon  Date <simondate@yahoo.co.uk>
2017-07-17 10:43:38 +0100

    Fixed issues with the PLP extension that were causing it not load in IE.

--- js/adapt-printPage.js
+++ js/adapt-printPage.js
@@ -5,13 +5,10 @@ define([
   var printPageView = Backbone.View.extend({
 
     initialize: function() {
-
-      this.listenTo(Adapt, 'remove', this.remove);
-      console.log(this.model.model._showOn);
-      if(this.model.model._showOn == 'navigation'){
+      if (this.model._showOn == 'navigation') {
         this.renderNavigation();
       } else {
-        var blocks = this.model.pageModel.findDescendants("blocks");
+        var blocks = Adapt.findById(Adapt.location._currentId).findDescendants('blocks');
         var lastID = blocks.last().get("_id");
         this.renderBottom(lastID);
       }
@@ -19,25 +16,25 @@ define([
 
     renderNavigation: function() {
       var template = Handlebars.templates["printPage-navigation"];
-      this.setElement(template(this.model.model)).$el.appendTo($(".navigation-inner"));
+      this.setElement(template(this.model)).$el.appendTo($(".navigation-inner"));
       this.listenTo(Adapt, {
-        "navigation:printPage": function(){
-          window.print();},
+        "navigation:printPage": function() {
+          window.print();
+        }
       });
     },
 
     renderBottom: function(lastID) {
       var template = Handlebars.templates["printPage-bottom"];
-      this.setElement(template(this.model.model)).$el.insertAfter($('.' + lastID));
-      return this;
+      this.setElement(template(this.model)).$el.insertAfter($('.' + lastID));
     }
   });
 
   Adapt.on("pageView:ready", function(pageView) {
-    var model = Adapt.findById(Adapt.location._currentId).attributes._printPage;
-    if(model == undefined) return;
-    if (model._isEnabled !== true) return;
-    var pageModel = pageView.model;
-    new printPageView({model:{ pageModel, model }});
+    var model = Adapt.findById(Adapt.location._currentId).get('_printPage');
+    if (model == undefined || !model._isEnabled) return;
+    new printPageView({
+      model: model
+    });
   });
 });
