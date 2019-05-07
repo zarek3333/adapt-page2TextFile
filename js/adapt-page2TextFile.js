ba3457257aa8400ec281dc54e1e86b7276290b5d
Mike Stevens <mesgraphix@gmail.com>
2019-05-07 10:31:50 -0400

    updated to page to text convert

--- js/adapt-page2TextFile.js
+++ js/adapt-page2TextFile.js
@@ -2,7 +2,7 @@ define([
 	'core/js/adapt'
 ], function(Adapt) {
 
-	var TimerDocservice = Backbone.View.extend({
+	var Page2TextFile = Backbone.View.extend({
 
 		initialize: function() {
 
@@ -18,47 +18,17 @@ define([
 	});
 
 	Adapt.once("adapt:initialize", function() {
-		var timercontrol = Adapt.course.get("_timerDocservice");
-		var selectimer = timercontrol._timerSelect;
-		var emaillink = timercontrol._emailContact;
-		var linkaddress = emaillink._emailAddress;
+		var pagetotext = Adapt.course.get("_page2TextFile");
+		var enablepg2txt = pagetotext._isEnabled;
 
 		$.getScript('libraries/FileSaver.js');
 
-		if (!timercontrol || !timercontrol._isEnabled) {
-			console.log("Timer is not enabled");
+		if (!enablepg2txt || !enablepg2txt._isEnabled) {
+			console.log("Page converter to text file is not enabled");
 		}
 
-		if (timercontrol && timercontrol._isEnabled) {
-			if (timercontrol && timercontrol._isLocked) {
-				$( '<div id="timer">' + selectimer + '</div>' ).appendTo( '#wrapper' );
-			} else {
-				$( '<div id="timer" class="nolocktime">' + selectimer + '</div>' ).appendTo( '#wrapper' );
-			}
-			console.log("Timer has been enabled");
-			//new TimerDocservice({ model: new Backbone.Model(button) });
-		}
-
-		if (!emaillink || !emaillink._emailEnabled) {
-			console.log("Email Address is not enabled");
-		}
-
-		if (emaillink && emaillink._emailEnabled) {
-			if (!timercontrol || !timercontrol._isEnabled) {
-				$( '<div id="myemail" style="right: 214px;"><a href="mailto:' + linkaddress + '" target="_blank">&nbsp;</a></div>' ).appendTo( '#wrapper' );
-				console.log("Email Address has been enabled");
-			} else {
-				$( '<div id="myemail"><a href="mailto:' + linkaddress + '" target="_blank">&nbsp;</a></div>' ).appendTo( '#wrapper' );
-				console.log("Email Address has been enabled");
-			}
-		}
-
-		if ( $('.menu').hasClass('tilesmenu-menu') ) {
-			$('#timer').addClass('timer4tile');
-			$('#myemail').addClass('email4tile');
-			console.log("Is Tile Menu");
-		} else {
-			console.log("Is Carousel Menu");
+		if (enablepg2txt && enablepg2txt._isEnabled) {
+			$( '<div id="mypage2txt" style="right: 314px;"><a href="#" target="_blank">&nbsp;</a></div>' ).appendTo( '#wrapper' );
 		}
 		
 	});
