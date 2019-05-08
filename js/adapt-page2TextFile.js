f586ecb94a143bdd9adaf7758ad571d1b2b5be18
Mike Stevens <mesgraphix@gmail.com>
2019-05-08 14:27:54 -0400

    Add files via upload

--- js/adapt-page2TextFile.js
+++ js/adapt-page2TextFile.js
@@ -1,6 +1,8 @@
 define([
-	'core/js/adapt'
-], function(Adapt) {
+	'backbone',
+    'coreJS/adapt',
+    /*'./adapt-notesView',*/
+], function(Backbone, Adapt/*, NotesView*/) {
 
 	var Page2TextFile = Backbone.View.extend({
 
@@ -19,17 +21,34 @@ define([
 
 	Adapt.once("adapt:initialize", function() {
 		var pagetotext = Adapt.course.get("_page2TextFile");
+		pagetotext = new Backbone.Model(pagetotext);
+		var accesspg2txt = pagetotext._ariaLabel;
 		var enablepg2txt = pagetotext._isEnabled;
 
 		$.getScript('libraries/FileSaver.js');
+		$.getScript('libraries/PageConverter.js');
 
-		if (!enablepg2txt || !enablepg2txt._isEnabled) {
-			console.log("Page converter to text file is not enabled");
-		}
+		// do not proceed until glossary enabled on course.json
+        if (!pagetotext || !pagetotext._isEnabled) {
+        	console.log("Page converter to text file is not enabled");
+            return;
+        }
 
-		if (enablepg2txt && enablepg2txt._isEnabled) {
-			$( '<div id="mypage2txt" style="right: 314px;"><a href="#" target="_blank">&nbsp;</a></div>' ).appendTo( '#wrapper' );
-		}
+        var template = Handlebars.templates['page2TextItem'];
+        var $element = $(template(pagetotext.toJSON()));
+
+        Adapt.drawer.triggerCustomView($element, false);
+
+        //sets up the drawer with config title and description
+        //shown to the student
+        /*var drawerObject = {
+            title: "Convert this page to text file",
+            description: "<script>$('.page2file-drawer').attr('onclick=\"totext()\"');</script>",
+            className: 'page2file-drawer'
+        };*/
+
+        // Syntax for adding a Drawer item
+	    /*Adapt.drawer.addItem(drawerObject, 'pageLevelProgress:show');*/
 		
 	});
 });
---
+++ libraries/PageConverter.js
@@ -0,0 +1,37 @@
+	function totext() { 
+		$snippets = $('.page');
+
+		$.each($snippets, function (index, obj) {
+	        var $this = $(this);
+	        $this.find('script').remove(':not(.accordion-item script):not(.notify-imagegrid-grid-item script)');
+	        $this.find('.toproceedon').remove();
+	        $this.find('.aria-label').remove();
+	        $this.find('.quicknav').remove();
+	        $this.find('.azure-widget script').remove();
+	        $this.find('.removeazurevid').remove();
+	        $this.find('.media-widget').remove();
+	        $this.find('.component-inner .buttons').remove();
+	        $this.find('.hotgraphic-popup-count').remove();
+	        $this.find('.matching-component script').remove();
+	        $this.find('.matching-select-container .select2').remove();
+	        $this.find('.narrative-component script').remove();
+	        $this.find('.reveal-widget-item .reveal-popup-open').remove();
+	        $this.find('.slider-scale-numbers .slider-scale-marker').remove();
+	        $this.find('.slider-widget .slider-background').remove();
+	        $this.find('.narrative-strapline-title .narrative-strapline-title-inner').before( "Mobile Narrative Strapline:" );
+
+			var $backofNotify = $this.text().replace(/\$\( \'\#accordalert/g, "Accordion Notify ").replace(/\' \)\.on\( \'click\'\, function\(\) \{ require\(\'coreJS\/adapt\'\)\.trigger\(\'notify\:alert\'\,\{title\:\'/g, " Title: ").replace(/\'\,body\:\'/g, "<br>Notify Body: ").replace(/\'\,confirmText\: \'Continue\'\}\)\;return false\; \}\)\;/g, "").replace(/\$\( \'img\[src\=\"course\/en\/assets\//g, "Image: ").replace(/\"\]\' \)\.addClass\(\'img2click\'\)\;/g, "<br>Image Grid Notify ").replace(/\"\]\' \)\.on\( \'click\'\, function\(\) \{/g, "").replace(/require\(\'coreJS\/adapt\'\)\.trigger\(\'notify\:popup\'\,\{title\:\'/g, " Title: ").replace(/\'\}\)\;return false\;/g, "").replace(/\}\)\;/g, "");
+	        
+	        //$this.html($backofNotify); //Creates page into plain text
+	        var txt = $.trim($backofNotify);
+        	var box = $('.page');
+        	box.val(box.val() + txt);
+
+	    });
+
+	var text = $('.page').val();
+    //var filename = $("#input-fileName").val();
+    var blob = new Blob([text], { type: "text/plain;charset=utf-8" });
+    //saveAs(blob, filename + ".txt"); 
+    saveAs(blob, "MikesDoc.txt");    
+	};
