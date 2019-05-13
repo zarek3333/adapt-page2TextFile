d55ff2dc42fc306caf4d3fca91cbda5fc6311412
Mike Stevens <mesgraphix@gmail.com>
2019-05-13 15:56:07 -0400

    Added .clone to protect page tidy table & bullets

--- libraries/PageConverter.js
+++ libraries/PageConverter.js
@@ -3,22 +3,23 @@ function totext() {
 
 		$.each($snippets, function (index, obj) {
 	        var $this = $(this);
-	        $this.find('script').remove(':not(.accordion-item script):not(.notify-imagegrid-grid-item script)');
-	        $this.find('.toproceedon').remove();
-	        $this.find('.aria-label').remove();
-	        $this.find('.quicknav').remove();
-	        $this.find('.azure-widget script').remove();
-	        $this.find('.removeazurevid').remove();
-	        $this.find('.media-widget').remove();
-	        $this.find('.component-inner .buttons').remove();
-	        $this.find('.hotgraphic-popup-count').remove();
-	        $this.find('.matching-component script').remove();
-	        $this.find('.matching-select-container .select2').remove();
-	        $this.find('.narrative-component script').remove();
-	        $this.find('.reveal-widget-item .reveal-popup-open').remove();
-	        $this.find('.slider-scale-numbers .slider-scale-marker').remove();
-	        $this.find('.slider-widget .slider-background').remove();
-	        $this.find('.narrative-strapline-title .narrative-strapline-title-inner').before( "Mobile Narrative Strapline:" );
+	        var clonedapage = $snippets.clone().prependTo( ".hidepgconvertxt" );
+	        clonedapage.find('script').remove(':not(.accordion-item script):not(.notify-imagegrid-grid-item script)');
+	        clonedapage.find('.toproceedon').remove();
+	        clonedapage.find('.aria-label').remove();
+	        clonedapage.find('.quicknav').remove();
+	        clonedapage.find('.azure-widget script').remove();
+	        clonedapage.find('.removeazurevid').remove();
+	        clonedapage.find('.media-widget').remove();
+	        clonedapage.find('.component-inner .buttons').remove();
+	        clonedapage.find('.hotgraphic-popup-count').remove();
+	        clonedapage.find('.matching-component script').remove();
+	        clonedapage.find('.matching-select-container .select2').remove();
+	        clonedapage.find('.narrative-component script').remove();
+	        clonedapage.find('.reveal-widget-item .reveal-popup-open').remove();
+	        clonedapage.find('.slider-scale-numbers .slider-scale-marker').remove();
+	        clonedapage.find('.slider-widget .slider-background').remove();
+	        clonedapage.find('.narrative-strapline-title .narrative-strapline-title-inner').before( "Mobile Narrative Strapline:" );
 
 	        //Image Grid Notifier find items using notifier
 	        $('.notify-imagegrid-grid-item').each(function() {
@@ -27,17 +28,26 @@ function totext() {
 		        }
 	        });
 
-	        $this.find('.griditemhasimg .notify-imagegrid-item-title').remove();
+	        clonedapage.find('.griditemhasimg .notify-imagegrid-item-title').remove();
+	        clonedapage.find('p').append('\n\n');
+	        clonedapage.find('ul').append('\n\n');
+	        clonedapage.find('ol').append('\n\n');
+	        clonedapage.find('ul ul li:first-child').prepend('\n\n\t');
+	        clonedapage.find('ol ol li:first-child').prepend('\n\n\t');
+	        clonedapage.find('li').append('\t');
+	        clonedapage.find('tr').append('\n\n');
+	        clonedapage.find('td').append('\t');
+	        clonedapage.find('th').append('\t');
 
-			var $backofNotify = $this.text().replace(/\$\( \'\#accordalert/g, "Accordion Notify ").replace(/\' \)\.on\( \'click\'\, function\(\) \{ require\(\'coreJS\/adapt\'\)\.trigger\(\'notify\:alert\'\,\{title\:\'/g, ": ").replace(/\'\,body\:\'/g, "\nNotify Body: ").replace(/\'\,confirmText\: \'Continue\'\}\)\;return false\; \}\)\;/g, "").replace(/\$\( \'img\[src\=\"course\/en\/assets\//g, "Notifier Image: ").replace(/\"\]\' \)\.addClass\(\'img2click\'\)\;/g, "").replace(/\"\]\' \)\.on\( \'click\'\, function\(\) \{/g, "").replace(/require\(\'coreJS\/adapt\'\)\.trigger\(\'notify\:popup\'\,\{title\:\'/g, "").replace(/\'\}\)\;return false\;/g, "").replace(/\}\)\;/g, "").replace(/\<p\>/g, " ").replace(/\<\/p\>/g, " ").replace(/\<p tabindex\=\"0\" class\=\"prevent-default accessible-text-block\" role=\"region\"\>/g, " ").replace(/\<p role\=\"region\" tabindex\=\"0\" class\=\"prevent-default accessible-text-block\"\>/g, " ");
+			var $backofNotify = clonedapage.text().replace(/\$\( \'\#accordalert/g, "Accordion Notify ").replace(/\' \)\.on\( \'click\'\, function\(\) \{ require\(\'coreJS\/adapt\'\)\.trigger\(\'notify\:alert\'\,\{title\:\'/g, ": ").replace(/\'\,body\:\'/g, "\nNotify Body: ").replace(/\'\,confirmText\: \'Continue\'\}\)\;return false\; \}\)\;/g, "").replace(/\$\( \'img\[src\=\"course\/en\/assets\//g, "Notifier Image: ").replace(/\"\]\' \)\.addClass\(\'img2click\'\)\;/g, "").replace(/\"\]\' \)\.on\( \'click\'\, function\(\) \{/g, "").replace(/require\(\'coreJS\/adapt\'\)\.trigger\(\'notify\:popup\'\,\{title\:\'/g, "").replace(/\'\}\)\;return false\;/g, "").replace(/\}\)\;/g, "").replace(/\<p\>/g, " ").replace(/\<\/p\>/g, " ").replace(/\<p tabindex\=\"0\" class\=\"prevent-default accessible-text-block\" role=\"region\"\>/g, " ").replace(/\<p role\=\"region\" tabindex\=\"0\" class\=\"prevent-default accessible-text-block\"\>/g, " ");
 	   
 	        var txt = $.trim($backofNotify);
-        	var box = $('.page');
+        	var box = $('.hidepgconvertxt');
         	box.val(box.val() + txt);
 
 	    });
 
-	var text = $('.page').val().replace(/\s\s+/g, '\n\n');
+	var text = $('.hidepgconvertxt').val().replace(/\s\s+/g, '\n\n');
     var blob = new Blob([text], { type: "text/plain;charset=utf-8" });
     var thepagination = $('.navpagenum').text();
     var thefilename = $('.modulehead').text();
